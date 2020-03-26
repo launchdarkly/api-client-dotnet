00f56253577277f2286017ea3600aa5087883f60
LaunchDarklyCI <team@launchdarkly.com>
2020-03-26 23:43:37 +0000

    Version 2.0.33 automatically generated from ld-openapi@3162f3d.

--- src/main/CsharpDotNet2/LaunchDarkly/Api/Client/Configuration.cs
+++ src/main/CsharpDotNet2/LaunchDarkly/Api/Client/Configuration.cs
@@ -123,7 +123,7 @@ namespace LaunchDarkly.Api.Client
                      .GetExecutingAssembly()
                      .GetReferencedAssemblies()
                      .Where(x => x.Name == "System.Core").First().Version.ToString()  + "\n";
-            report += "    Version of the API: 2.0.32\n";
+            report += "    Version of the API: 2.0.33\n";
             report += "    SDK Package Version: 1.0.0\n";
   
             return report;
